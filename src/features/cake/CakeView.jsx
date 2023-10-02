e3f147ce0e40ae6a2badec439555222380ca3a21
illya-onyshchuk <llayonys@gmail.com>
2023-10-03 00:04:12 +0300

    add store

--- src/features/cake/CakeView.jsx
+++ src/features/cake/CakeView.jsx
@@ -1,7 +1,12 @@
+import { useSelector } from "react-redux";
+
+
 export const CakeView = () => {
+  const nummerOfCakes = useSelector((state) => state.cake.numOfCake);
+  
   return (
     <div>
-      <h2>Number of cakes -</h2>
+      <h2>Number of cakes - {nummerOfCakes}</h2>
       <button>Order cake</button>
       <button>Restock cakes</button>
     </div>
--- src/features/icecream/IcecreamView.jsx
+++ src/features/icecream/IcecreamView.jsx
@@ -1,7 +1,11 @@
+import { useSelector } from "react-redux";
+
 export const IcecreamView = () => {
+  const icecream = useSelector(state => state.icecream.icecream)
+
   return (
     <div>
-      <h2>Number of Ice cream -</h2>
+      <h2>Number of Ice cream - {icecream}</h2>
       <button>Order Ice cream</button>
       <button>Restock Ice creams</button>
     </div>
